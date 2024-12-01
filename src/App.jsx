1873024c7bb02cc654a991503c9111d9440eb982
Abhishek Dhangekar <abhidhangekar@gmail.com>
2024-12-01 22:40:31 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// App.jsx
+import React from 'react';
+import ServicesPage from './ServicesPage';
+
+function App() {
+  return (
+    <div className="App">
+      <ServicesPage />
+    </div>
+  );
+}
+
+export default App;
